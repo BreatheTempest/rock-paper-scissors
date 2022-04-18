1051593f2c5a8d52478e11c1289f96bdb7bbae12
Anton Zabulonov <zabulonov.a@gmail.com>
2022-04-18 22:13:28 +0300

    Add more styling and rules

--- script.js
+++ script.js
@@ -4,61 +4,62 @@ function computerPlay() {
 }
 
 function round(computerPlay, playersChoice) {
-	console.log(`Computer: ${computerPlay}, Player: ${playersChoice}.`);
+	console.log(`Computer: ${computerPlay}`);
+	console.log(`Player: ${playersChoice}`);
 	if (computerPlay === playersChoice) {
 		console.log(
-			'%c Draw!',
-			'font-size:12px; background-color:blueviolet; color:white; padding:8px; border-radius:50px'
+			'%cDraw!',
+			'font-size:12px; background-color:dimgray; color:white; padding:8px; border-radius:50px'
 		);
 		return 3;
 	}
 	if (computerPlay === 'rock' && playersChoice === 'paper') {
 		console.log(
-			'%c You win! Paper beats Rock',
-			'font-size:12px; background-color:darkgreen; color:white; padding:8px; border-radius:50px'
+			'%cYou win! Paper beats Rock',
+			'font-size:12px; background-color:#417a41; color:white; padding:8px; border-radius:50px'
 		);
 		return 1;
 	}
 	if (computerPlay === 'rock' && playersChoice === 'scissors') {
 		console.log(
-			'%c You lose! Rock beats Scissors',
-			'font-size:12px; background-color:crimson; color:white; padding:8px; border-radius:50px'
+			'%cYou lose! Rock beats Scissors',
+			'font-size:12px; background-color:#dc546f; color:white; padding:8px; border-radius:50px'
 		);
 		return 2;
 	}
 	if (computerPlay === 'paper' && playersChoice === 'scissors') {
 		console.log(
-			'%c You win! Scissors beats Paper',
-			'font-size:12px; background-color:darkgreen; color:white; padding:8px; border-radius:50px'
+			'%cYou win! Scissors beats Paper',
+			'font-size:12px; background-color:#417a41; color:white; padding:8px; border-radius:50px'
 		);
 		return 1;
 	}
 
 	if (computerPlay === 'paper' && playersChoice === 'rock') {
 		console.log(
-			'%c You lose! Paper beats Rock',
-			'font-size:12px; background-color:crimson; color:white; padding:8px; border-radius:50px'
+			'%cYou lose! Paper beats Rock',
+			'font-size:12px; background-color:#dc546f; color:white; padding:8px; border-radius:50px'
 		);
 		return 2;
 	}
 
 	if (computerPlay === 'scissors' && playersChoice === 'paper') {
 		console.log(
-			'%c You lose! Scissors beats Paper',
-			'font-size:12px; background-color:crimson; color:white; padding:8px; border-radius:50px'
+			'%cYou lose! Scissors beats Paper',
+			'font-size:12px; background-color:#dc546f; color:white; padding:8px; border-radius:50px'
 		);
 		return 2;
 	}
 	if (computerPlay === 'scissors' && playersChoice === 'rock') {
 		console.log(
-			'%c You win! Rock beats Scissors',
-			'font-size:12px; background-color:darkgreen; color:white; padding:8px; border-radius:50px'
+			'%cYou win! Rock beats Scissors',
+			'font-size:12px; background-color:#417a41; color:white; padding:8px; border-radius:50px'
 		);
 		return 1;
 	}
 
 	console.log(
-		'%c Wrong word! Choose Rock, Paper or Scissors',
+		'%cWrong word! Choose Rock, Paper or Scissors',
 		'font-size:12px; background-color:sienna; color:white; padding:8px; border-radius:50px'
 	);
 	return 0;
@@ -66,18 +67,20 @@ function round(computerPlay, playersChoice) {
 
 function game() {
 	console.log(
-		'%c New game!',
-		'font-size:30px; background-color:#333; color:white; padding:8px 16px; border-radius:50px'
+		'%cNew game!',
+		'font-size:30px; background-color:#333; color:white; padding:8px 16px; border-radius:50px;'
 	);
 
 	let points = 0;
 	for (let i = 0; i < 5; i++) {
 		let computer = computerPlay();
-		let player = prompt('Choose: Rock, Paper or Scissors').toLowerCase();
-		console.log(
-			`%c Round #${i + 1}!`,
-			'font-size:15px; background-color:sienna; color:white; padding:8px; border-radius:50px'
-		);
+		let player = prompt('Choose: Rock, Paper or Scissors');
+		if (player === null) {
+			alert('Canceled! Press Play to try again!');
+			return;
+		}
+		player = player.toLowerCase();
+		console.log(`%cRound #${i + 1}!`, 'font-size:15px; ');
 		let score = round(computer, player);
 		if (score === 0) i--;
 		else if (score === 1) points++;
@@ -85,18 +88,18 @@ function game() {
 	}
 	if (points === 0)
 		console.log(
-			"%c It's a Draw!",
-			'font-size:15px; background-color:blueviolet; color:white; padding:8px; border-radius:50px'
+			"%cIt's a Draw!",
+			'font-size:20px; background-color:#494949; color:white; padding:16px; border-radius:50px; text-align:center'
 		);
 	if (points > 0)
 		console.log(
-			'%c Congratulation, you won!',
-			'font-size:15px; background-color:darkgreen; color:white; padding:8px; border-radius:50px'
+			'%cCongratulation, you won!',
+			'font-size:20px; background-color:darkgreen; color:white; padding:16px; border-radius:50px; text-align:center'
 		);
 	if (points < 0)
 		console.log(
-			"%c I'm sorry, you lost!",
-			'font-size:15px; background-color:crimson; color:white; padding:8px; border-radius:50px'
+			"%cI'm sorry, you lost!",
+			'font-size:20px; background-color:crimson; color:white; padding:16px; border-radius:50px; text-align:center'
 		);
 }
 
